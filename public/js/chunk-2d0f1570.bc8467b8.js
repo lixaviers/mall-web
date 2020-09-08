351410a69e61acd50a50ca06d269d77944bccf38
xavier <xavier.li@staples.cn>
2020-09-08 11:08:18 +0800

    发布

---
+++ public/js/chunk-2d0f1570.bc8467b8.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0f1570"],{"9fe2":function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("404")])},u=[],l=t("9ca4"),a={},r=Object(l["a"])(a,c,u,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d0f1570.bc8467b8.js.map
--- src/views/member.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        <h1>member</h1>
-        <h1>member</h1>
-    </div>
-</template>
